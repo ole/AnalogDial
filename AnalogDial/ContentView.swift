47686967f1da1d066d175ee19cdae3b08c52591c
Ole Begemann <ole@oleb.net>
2019-06-21 21:57:37 +0200

    Initial Commit

---
+++ AnalogDial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AnalogDial
+//
+//  Created by Ole Begemann on 21.06.19.
+//  Copyright © 2019 Ole Begemann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
