24ff83023b8cc47958eafb63685dbe42adab93b3
Ole Begemann <ole@oleb.net>
2019-06-22 15:42:42 +0200

    Documentation

--- AnalogDial/ContentView.swift
+++ AnalogDial/ContentView.swift
@@ -12,26 +12,69 @@ struct ContentView: View {
   }
 }
 
+/// A circular analog dial that can display a numeric value in a specified range (like an analog speedometer)
+///
+///          .───────.
+///        ,'   30    `.
+///      ,'             `.
+///     ;   20       40   :
+///     │                 │
+///     │  10──────    50 │
+///     :                 ;
+///      ╲   0       60  ╱
+///       `.           ,'
+///          `───────'`
+///
 struct AnalogDial: View {
   @Binding var currentValue: Double
 
+  /// The minimum value in the dial's range
   let minValue: Double = 0
+  /// The maximum value in the dial's range
   let maxValue: Double = 60
+  /// Step size between major tick marks
   let majorStep: Double = 10
+  /// Number of subdivisions (minor ticks) between major tick marks.
   let subdivisions = 4
-  let startAngle = Angle(degrees: -220)
-  let endAngle = Angle(degrees: 40)
+
+  /// The angle where the dial scale should start.
+  ///
+  /// Measured in SwiftUI's default coordinate system, i.e. 0º is "east" (positive x axis) and positive angles go clockwise.
+  /// The `startAngle` must be smaller than `endAngle`. Use a negative angle to go counterclockwise from east.
+  /// Examples:
+  /// - 0º is "east" (straight right from the view's center)
+  /// - -90º is straight up from the view's center (I avoid "north" and "south" because it's confusing. SwiftUI's y axis is
+  ///   flipped, so would "north" mean up or down?)
+  /// - -180º is "west" (straight left from the view's center)
+  /// - -225º is the bottom-left "corner" of the dial.
+  /// - 45º is the bottom-right "corner" of the dial.
+  /// - 90º is straight down from the view's center, as is -270º. If you set startAngle to -270º and endAngle to 90º, the
+  ///   dial would span the full circle.
+  let startAngle = Angle(degrees: -225)
+
+  /// The angle where the dial scale should end.
+  ///
+  /// Measured in SwiftUI's default coordinate system, i.e. 0º is "east" (positive x axis) and positive angles go clockwise.
+  /// The `startAngle` must be smaller than `endAngle`. Use a negative angle to go counterclockwise from east.
+  /// E.g. An angle of -180º is "west" (negative x axis) and -225º is "south-west".
+  let endAngle = Angle(degrees: 45)
+
+  // TODO: These should probably be SwiftUI modifiers (or styles?).
+  /// The dial's background color.
   let backgroundColor = Color.black
+  /// The color of the dial border. Use `Color.clear` to not draw a border.
+  let borderColor = Color.clear
   let textColor = Color.white
   let tickMarkColor = Color.white
+  /// The color of the dial's hand.
   let handColor = Color.orange
-  let borderColor = Color.clear
 
-  let majorTicks: [Double]
-  let minorTicks: [Double]
+  private let majorTicks: [Double]
+  private let minorTicks: [Double]
 
   init(_ value: Binding<Double>) {
     $currentValue = value
+    // Compute the dial values where we must draw major and minor tick marks and labels.
     let majorTicks = Array(stride(from: minValue, through: maxValue, by: majorStep))
     let subdivisions = self.subdivisions
     let majorStep = self.majorStep
@@ -74,6 +117,7 @@ struct AnalogDial: View {
       .accessibility(value: Text("\(currentValue)"))
   }
 
+  /// Returns the angle that corresponds with the given value.
   private func angle(for value: Double) -> Angle {
     let normalized = interpolate(value, min: minValue, max: maxValue)
     return startAngle + (endAngle - startAngle) * normalized
@@ -174,11 +218,12 @@ struct AnalogDial: View {
       }
     }
 
-    static let scaleFactorX: CGFloat = 0.5
+    static let scaleFactorX: CGFloat = 0.45
     static let scaleFactorY: CGFloat = 0.01
-    /// The position of the pivot point, relative to its main axis.
+    /// The position of the hand's pivot point, relative to the hand's long axis.
     /// Must be between 0 and 1. A value of 0.5 means that the hand rotates around its center.
-    static let pivotPosition: CGFloat = 0.2
+    static let pivotPosition: CGFloat = 0.1
+    /// The relative size of the "knob" in the center of the dial (where the hand pivot is attached).
     static let knobScaleFactor: CGFloat = 0.03
   }
 }
@@ -193,15 +238,21 @@ struct ContentView_Previews : PreviewProvider {
 }
 #endif
 
+/// Linear interpolation between two values.
 func interpolate(_ value: Double, min: Double, max: Double) -> Double {
   (value - min) / (max - min)
 }
 
-/// Polar coordinate
+/// A polar coordinate (angle and radius).
+///
+/// Note: The angle of a polar coordinate (`phi`) goes counterclockwise, i.e. 0º is "east" and 90º is "north".
+/// This is also true here but note that SwiftUI's y axis is flipped (positive values go down), so "north" in SwiftUI's
+/// coordinate system is down. If you need an angle in the upper-right or upper-left quadrant, use a negative angle.
 struct Polar {
   var r: Length
   var phi: Angle
 
+  /// Converts a polar coordinate to its corresponding cartesian coordinate (x and y position).
   var cartesian: CGPoint {
     // x = r * cos(phi)
     // y = r * sin(phi)
